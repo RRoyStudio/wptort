c3d5aa1b748c1dacf2d9a4d08db4642f8e49c596
RRoyStudio <roy.rebecca98@gmail.com>
2024-10-14 14:32:43 -0500

    1.18.03 in video

--- Wordpress-tem/functions.php
+++ Wordpress-tem/functions.php
@@ -1,5 +1,22 @@
 <?php
 
+function followbec_theme_support(){
+    //adds dynamic header support as long as you have wp_header in front page
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+}
+add_action('after_setup_theme', 'followbec_theme_support');
+
+function followbec_menus(){
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidbar",
+        'footer' => "Footer Menu"
+    );
+register_nav_menus($locations);
+}
+add_action('init', 'followbec_menus');
+
+
 
 function followbec_register_styles(){
         $version = wp_get_theme()->get('Version');
@@ -12,7 +29,10 @@ add_action('wp_enqueue_scripts','followbec_register_styles');
 
 
 function followbec_register_scripts(){
-    wp_enqueue_script('follow-bec-bootstrap','followbec_register_scripts','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(),'3.4.1', true);
+    wp_enqueue_script('follow-bec-jquery','followbec_register_scripts','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(),'3.4.1', true);
+    wp_enqueue_script('follow-bec-popper','followbec_register_scripts',"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(),'1.16.0', true);
+    wp_enqueue_script('follow-bec-bootstrap','followbec_register_scripts',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(),'4.4.1', true);
+    wp_enqueue_script('follow-bec-main','followbec_register_scripts', get_template_directory_uri() . "/assets/js/main.js", array(),'1.0', true);
 };
 
 add_action('wp_enqueue_scripts', 'followbec_register_scripts')
